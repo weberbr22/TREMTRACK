e74e7ab8375713c5ff943b396234b74d5afa53ca
weberbr22 <113095393+weberbr22@users.noreply.github.com>
2022-11-06 12:06:20 +0000

    ff

--- TREMTRACK.ino
+++ TREMTRACK.ino
@@ -1,6 +1,11 @@
 #include<Wire.h>
+
 const int MPU=0x68; 
 int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
+
+int red_light_pin= 4;
+int green_light_pin = 5;
+int blue_light_pin = 6;
 
 void setup(){
   Wire.begin();
@@ -9,8 +14,73 @@ void setup(){
   Wire.write(0);    
   Wire.endTransmission(true);
   Serial.begin(9600);
+
+  pinMode(red_light_pin, OUTPUT);
+  pinMode(green_light_pin, OUTPUT);
+  pinMode(blue_light_pin, OUTPUT);
 }
+
+//void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
+ //{
+  //analogWrite(red_light_pin, red_light_value);
+  //analogWrite(green_light_pin, green_light_value);
+  //analogWrite(blue_light_pin, blue_light_value);
+//}
+
 void loop(){
+  //RGB_color(255, 0, 0); // Red
+  //delay(1000);
+  //RGB_color(0, 255, 0); // Green
+  //delay(1000);
+  //RGB_color(0, 0, 255); // Blue
+  //delay(1000);
+  //RGB_color(255, 255, 125); // Raspberry
+  //delay(1000);
+  //RGB_color(0, 255, 255); // Cyan
+  //delay(1000);
+  //RGB_color(255, 0, 255); // Magenta
+  //delay(1000);
+  //RGB_color(255, 255, 0); // Yellow
+  //delay(1000);
+  //RGB_color(255, 255, 255); // White
+  //delay(1000);
+
+  //int buzzer = 10;          
+
+  //tone(buzzer, 262, 250);   
+  //delay(325);              
+  //noTone(buzzer);           
+
+  //tone(buzzer, 196, 125);
+  //delay(162);
+  //noTone(buzzer);
+
+  //tone(buzzer, 196, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //tone(buzzer, 220, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //tone(buzzer, 196, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //tone(buzzer, 0, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //tone(buzzer, 247, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //tone(buzzer, 262, 250);
+  //delay(325);
+  //noTone(buzzer);
+
+  //delay(1000);
+
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);  
   Wire.endTransmission(false);
@@ -34,3 +104,4 @@ void loop(){
   Serial.println(" ");
   delay(333);
 }
+
